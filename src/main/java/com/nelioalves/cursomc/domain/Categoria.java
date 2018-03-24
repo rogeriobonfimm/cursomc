2929d8e4f3bba4cc6064b5477217df0fb1f6ac2c
Rogerio Vilela Bonfim <rogerio_vilela2007@hotmail.com>
2018-03-24 06:58:40 -0700

    Creating the many to many relationship at Categoria and Product classed

--- src/main/java/com/nelioalves/cursomc/domain/Categoria.java
+++ src/main/java/com/nelioalves/cursomc/domain/Categoria.java
@@ -1,21 +1,28 @@
 package com.nelioalves.cursomc.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 
 @Entity
 public class Categoria implements Serializable
 {
 	private static final long serialVersionUID = 1L;
+	
 	@Id
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Integer id;
 	private String nome;
 	
+	@ManyToMany(mappedBy="categorias")
+	private List<Produto> produtos = new ArrayList<>();
+
 	public Categoria()
 	{	
 	}
@@ -43,6 +50,14 @@ public class Categoria implements Serializable
 		this.nome = nome;
 	}
 
+	public List<Produto> getProdutos() {
+		return produtos;
+	}
+
+	public void setProdutos(List<Produto> produtos) {
+		this.produtos = produtos;
+	}
+	
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -67,6 +82,5 @@ public class Categoria implements Serializable
 			return false;
 		return true;
 	}
-	
 	
 }
